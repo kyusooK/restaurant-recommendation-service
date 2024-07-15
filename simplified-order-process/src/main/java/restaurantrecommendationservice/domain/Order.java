61c21a7b870f118833106fa3f4b8a51eaaf45c9f
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-07-15 10:47:16 +0900

    Correct: 테스트 코드에서 getRestaurant() 메소드를 호출하지만 OrderPlaced 클래스에 해당 매서드가 없어 발생하는 에러 해결

--- simplified-order-process/src/main/java/restaurantrecommendationservice/domain/Order.java
+++ simplified-order-process/src/main/java/restaurantrecommendationservice/domain/Order.java
@@ -1,48 +1,5 @@
 package restaurantrecommendationservice.domain;
+...existing code... 
+public void setRestaurant(String restaurantName) { this.restaurantName = restaurantName; }
 
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-import javax.persistence.*;
-import lombok.Data;
-import restaurantrecommendationservice.SimplifiedOrderProcessApplication;
-import restaurantrecommendationservice.domain.OrderPlaced;
-
-@Entity
-@Table(name = "Order_table")
-@Data
-//<<< DDD / Aggregate Root
-public class Order {
-
-    @Id
-    private String orderId;
-
-    private String restaurantName;
-
-    private Address deliveryAddress;
-
-    @Embedded
-    private String restaurantId;
-
-    @PostPersist
-    public void onPostPersist() {
-        OrderPlaced orderPlaced = new OrderPlaced(this);
-        orderPlaced.publishAfterCommit();
-    }
-
-    @PrePersist
-    public void onPrePersist() {
-        // 비즈니스 로직 추가
-        if (this.restaurantName == null || this.deliveryAddress == null) {
-            throw new IllegalArgumentException("All fields must be supplied");
-        }
-    }
-
-    public static OrderRepository repository() {
-        OrderRepository orderRepository = SimplifiedOrderProcessApplication.applicationContext.getBean(
-            OrderRepository.class
-        );
-        return orderRepository;
-    }
-}
-//>>> DDD / Aggregate Root
+...existing code...
--- simplified-order-process/src/main/java/restaurantrecommendationservice/domain/OrderPlaced.java
+++ simplified-order-process/src/main/java/restaurantrecommendationservice/domain/OrderPlaced.java
@@ -1,26 +1,4 @@
 package restaurantrecommendationservice.domain;
-
-import java.time.LocalDate;
-import java.util.*;
-import lombok.*;
-import restaurantrecommendationservice.domain.*;
-import restaurantrecommendationservice.infra.AbstractEvent;
-
-//<<< DDD / Domain Event
-@Data
-@ToString
-public class OrderPlaced extends AbstractEvent {
-
-    private String orderId;
-    private String restaurantName;
-    private Address deliveryAddress;
-
-    public OrderPlaced(Order aggregate) {
-        super(aggregate);
-    }
-
-    public OrderPlaced() {
-        super();
-    }
-}
-//>>> DDD / Domain Event
+...existing code... 
+public String getRestaurant() { return this.restaurantName; }
+...existing code...
