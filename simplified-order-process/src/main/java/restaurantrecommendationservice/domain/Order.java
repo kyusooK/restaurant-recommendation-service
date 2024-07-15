c9eef3dbe64f89175d095bffdce87b1d816e2267
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-07-15 10:59:28 +0900

    문법 수정: 세미콜론이 없어서 생긴 문제로 코드 줄 끝에 ';'가 있는지 확인하십시오.

--- simplified-order-process/src/main/java/restaurantrecommendationservice/domain/Order.java
+++ simplified-order-process/src/main/java/restaurantrecommendationservice/domain/Order.java
@@ -1,5 +1,1 @@
-package restaurantrecommendationservice.domain;
-...수정된 코드... 
-public class Order { public void setRestaurant(String restaurantName) { this.restaurantName = restaurantName; }
-
-...수정된 코드...}
+...수정된 코드...
--- simplified-order-process/src/main/java/restaurantrecommendationservice/domain/OrderPlaced.java
+++ simplified-order-process/src/main/java/restaurantrecommendationservice/domain/OrderPlaced.java
@@ -1,5 +1,1 @@
-package restaurantrecommendationservice.domain;
-...수정된 코드... 
-public class OrderPlaced { public String getRestaurant() { return this.restaurantName; }
-
-...수정된 코드...}
+...수정된 코드...
